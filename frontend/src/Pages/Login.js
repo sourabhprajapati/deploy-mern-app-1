e31ed7208c5ab268cfd2120b3098fdff6bf8872c
sourabhprajapati <50737649+sourabhprajapati@users.noreply.github.com>
2025-03-20 21:53:01 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
             return handleError('email and password are required ')
           }
           try {
-            const url="http://localhost:8080/auth/login";
+            const url="https://deploy-mern-app-1-api-delta.vercel.app/auth/login";
             const response=await fetch(url,{
                 method:'POST',
                 headers:{
